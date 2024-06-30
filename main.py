c902e92704b7c65328f201472bd76ff002322ec2
Filip <filip.zalozyk@gmail.com>
2024-06-30 11:17:11 +0200

    add words counting and a little code refactoring

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+def main():
+    print(count_words(show_contents()))
+
+def count_words(target):
+    words = target.split()
+    result = len(words)
+    return result
+
+def show_contents():
+    path = "books/frankenstein.txt"
+    with open(path, 'r') as f:
+        return f.read()
+
+if __name__ == "__main__":
+    main()
